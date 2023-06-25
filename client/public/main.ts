0143ba8ea1d5981728d940894e21adf0450d8928
AVitrum <andrey.almashi@gmail.com>
2023-06-25 23:27:51 +0300

    Converted main.js to main.ts

---
+++ client/public/main.ts
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    mainWindow.loadURL('http://localhost:3000');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
